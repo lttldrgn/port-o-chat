7a958d566647a5582f77bf39446630b615480b60
lttldrgn@gmail.com <lttldrgn@gmail.com@30b60c88-fbde-8d45-46a2-ccc87e254847>
2011-06-09 04:14:47 +0000

    Starting to beef up ServerConnection

--- src/portochat/client/ServerConnection.java
+++ src/portochat/client/ServerConnection.java
@@ -25,17 +25,26 @@ public class ServerConnection {
     private Socket socket = null;
     private PrintWriter outWriter = null;
     private BufferedReader reader = null;
+    private volatile boolean connected = false;
     
     public ServerConnection() {
         
     }
     
-    public boolean connectToServer(String serverAddress) {
+    public synchronized boolean connectToServer(String serverAddress) {
+        if (connected) {
+            return false;
+        }
+        
         boolean successful = true;
         try {
             socket = new Socket(serverAddress, ClientSettings.DEFAULT_SERVER_PORT);
-            outWriter = new PrintWriter(socket.getOutputStream());
+            outWriter = new PrintWriter(socket.getOutputStream(), true);
             reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            connected = true;
+            IncomingProcessingThread incomingThread = new IncomingProcessingThread();
+            incomingThread.start();
+            
         } catch (UnknownHostException ex) {
             logger.log(Level.SEVERE, null, ex);
             successful = false;
@@ -46,25 +55,48 @@ public class ServerConnection {
         return successful;
     }
     
-    public void sendPrivateMessage(String username, String message) {
+    public synchronized void disconnectFromServer() {
+        try {
+            outWriter.close();
+            reader.close();
+            socket.close();
+            connected = false;
+        } catch (IOException ex) {
+            logger.log(Level.WARNING, null, ex);
+        }
+    }
+    
+    public synchronized void sendPrivateMessage(String username, String message) {
         
     }
     
-    public void joinChannel(String channel) {
+    public synchronized void joinChannel(String channel) {
         outWriter.println(ProtocolDefinitions.JOIN_CHANNEL + channel);
     }
     
-    public void leaveChannel(String channel) {
+    public synchronized void leaveChannel(String channel) {
         outWriter.println(ProtocolDefinitions.LEAVE_CHANNEL + channel);
     }
     
-    public ArrayList<String> channelWho(String channel) {
+    public synchronized ArrayList<String> channelWho(String channel) {
         outWriter.println(ProtocolDefinitions.LEAVE_CHANNEL + channel);
-        try {
-            reader.readLine();
-        } catch (IOException ex) {
-            logger.log(Level.SEVERE, null, ex);
-        }
+
         return new ArrayList<String>();
+    }
+    
+    private class IncomingProcessingThread extends Thread {
+        public IncomingProcessingThread() {
+            super("Incoming processing thread");
+        }
+        
+        public void run() {
+            while (connected) {
+                try {
+                    System.out.println(reader.readLine());
+                } catch (IOException ex) {
+                    logger.log(Level.SEVERE, null, ex);
+                }
+            }
+        }
     }
 }
