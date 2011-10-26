ac7e4c9f17f24edf468551ddb9500cad8e60340d
Brandon <lttldrgn@users.noreply.github.com>
2011-10-26 04:58:42 +0000

    [Issue 42] Refactored to only use keyPressed event.  Finally figured out that Microsoft IME uses the "Enter" key to confirm the character selection.  This does not trigger a keyPressed event, but was being picked up by the keyReleased method and clearing the input selection.  Removed the hack put in the last revision and fixed the faulty logic overall.

--- src/portochat/client/ChatPane.java
+++ src/portochat/client/ChatPane.java
@@ -140,27 +140,11 @@ public class ChatPane extends JPanel implements PropertyChangeListener {
                 if (e.getKeyCode() == KeyEvent.VK_ENTER) {
 
                     String text = textEntry.getText();
-                    if (text.isEmpty() || text.startsWith("\n")) {
-                        textEntry.setText("");
-                    } else {
+                    if (!text.isEmpty()) {
                         processInputMessage(text);
+                        textEntry.setText("");
                     }
                     e.consume();
-                }
-            }
-            
-            @Override
-            public void keyReleased(KeyEvent e) {
-                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
-                    // Added this check here for when MS IME is being used
-                    // with some character sets and for some reason we do not 
-                    // get the key pressed events, but we do get key released 
-                    // events so we can catch the input here
-                    String text = textEntry.getText();
-                    if (!text.isEmpty() && !text.startsWith("\n")) {
-                        processInputMessage(text);
-                    }
-                    textEntry.setText("");
                 }
             }
         });
@@ -593,7 +577,6 @@ public class ChatPane extends JPanel implements PropertyChangeListener {
                 logger.log(Level.SEVERE, "Error appending", ioe);
             }
             sendMessage(action, message);
-            textEntry.setText("");
         }
     }
     
