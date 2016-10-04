ee7833018752b72c8a8155c49a0caff525eeffd8
Brandon <lttldrgn@users.noreply.github.com>
2016-10-04 00:38:03 -0400

    #64 Update to Gradle 3.1

--- scripts/client.sh
+++ scripts/client.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Djava.util.logging.config.file=logging.properties -cp port-o-chat-1.0.jar portochat.client.ClientLauncher
+java -Djava.util.logging.config.file=logging.properties -cp port-o-chat-1.0.jar com.lttldrgn.portochat.client.ClientLauncher
